2dc5db524af28388869253726a2e7c685024b56f
sachin kumar <sachinmehtab@gmail.com>
2024-10-03 22:18:38 +0530

    intro

---
+++ src/main/java/sachin/coding_ninja_in_order/Average.java
@@ -0,0 +1,34 @@
+
+
+package sachin.coding_ninja_in_order;
+
+public class Average{
+
+    public static double averageOfNumbers(int a , int b ,int c) {
+         double ans = (a+b+c)/3.0;
+         return ans;
+    }
+
+
+   public static void main( String  args []){
+      
+      System.out.println(averageOfNumbers(1, 2, 3));
+   }
+
+}
+
+
+/*
+
+3 syntax of creating array
+
+// spread operator - to learn
+
+string [] arr
+string arr []
+
+
+String is not premitive (pascal casing )
+int  is primitive (first char is small)
+
+*/
