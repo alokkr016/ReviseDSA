cb599cffe4dd3d99134bd8d14d555891a645ad23
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2024-10-04 02:27:06 +0530

    Check the type of traingle

--- src/main/java/alok/introduction/CheckTriangle.java
+++ src/main/java/alok/introduction/CheckTriangle.java
@@ -5,12 +5,54 @@ import java.util.Scanner;
 public class CheckTriangle {
 
     public static void checkTypeOfTriangle(double side1, double side2, double side3) {
+        if (!ValidTriangle.isValidTriangle(side1, side2, side3)) {
+            System.out.println("The triangle is not valid");
+            return;
+        }
+
         if (side1 == side2 && side2 == side3) {
             System.out.println("equilateral");
-        } else if (side1 == side2 || side2 == side3) {
+        } else if (side1 == side2 || side2 == side3 || side1 == side3) {
             System.out.println("isosceles");
-        }else{
+        } else {
             System.out.println("scalene");
+        }
+    }
+
+    public static double findMax(double a, double b, double c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void checkTriangleType(double side1, double side2, double side3) {
+        if (!ValidTriangle.isValidTriangle(side1, side2, side3)) {
+            System.out.println("The triangle is not valid");
+            return;
+        }
+
+        double maxSide = findMax(side1, side2, side3);
+
+        if (maxSide == side1) {
+            checkTriangle(side1, side2, side3);
+        } else if (maxSide == side2) {
+            checkTriangle(side2, side1, side3);
+        } else {
+            checkTriangle(side3, side3, side1);
+        }
+    }
+
+    public static void checkTriangle(double maxSide, double otherSide1, double otherMaxSide2) {
+        if (maxSide * maxSide == (otherSide1 * otherSide1) + (otherMaxSide2 * otherMaxSide2)) {
+            System.out.println("Right angle triangle");
+        } else if ((maxSide * maxSide) > (otherSide1 * otherSide1) + (otherMaxSide2 * otherMaxSide2)) {
+            System.out.println("Obtuse angle triangle");
+        } else {
+            System.out.println("acute angle triangle");
         }
     }
 
@@ -19,6 +61,7 @@ public class CheckTriangle {
         double side1 = in.nextDouble();
         double side2 = in.nextDouble();
         double side3 = in.nextDouble();
-        checkTypeOfTriangle(side1,side2,side3);
+        checkTriangleType(side1,side2,side3);
+        checkTypeOfTriangle(side1, side2, side3);
     }
 }
