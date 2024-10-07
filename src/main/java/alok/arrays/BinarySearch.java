0cea9fed1b93a1d186b16b9893844826297f247f
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2024-10-08 00:18:38 +0530

    Binary search

--- src/main/java/alok/arrays/BinarySearch.java
+++ src/main/java/alok/arrays/BinarySearch.java
@@ -2,8 +2,16 @@ package alok.arrays;
 
 public class BinarySearch {
 
-    public static int binarySearchIterative(int arr[], int x) {
-        int left = 0, right = arr.length - 1;
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(binarySearch(arr, 2));
+        System.out.println(binarySearch(arr,3,0,arr.length - 1));
+    }
+
+    private static int binarySearch(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (arr[mid] == x) {
@@ -18,29 +26,19 @@ public class BinarySearch {
         return -1;
     }
 
-    public static int binarySearchRecursive(int arr[], int x) {
-        return binarySearchRecursive(arr, x, 0, arr.length - 1);
-    }
-
-    private static int binarySearchRecursive(int[] arr, int x, int left, int right) {
-        if(left > right){
-            return -1;
+    private static int binarySearch(int[] arr, int x, int left, int right) {
+        if (left <= right) {
+            int mid = (left) + (right - left) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] > x) {
+                right = mid - 1;
+                return binarySearch(arr, x, left, right);
+            } else {
+                left = mid + 1;
+                return binarySearch(arr, x, left, right);
+            }
         }
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] < x) {
-            left = mid + 1;
-            return binarySearchRecursive(arr, x, left, right);
-        } else if (arr[mid] == x) {
-            return mid;
-        } else {
-            right = mid - 1;
-            return binarySearchRecursive(arr, x, left, right);
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-        System.out.println(binarySearchRecursive(arr, 9));
+        return -1;
     }
 }
